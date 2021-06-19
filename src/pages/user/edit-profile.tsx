e450401726f03aa790a2a1aaa91f98843f69cd9d
Daniel <ssh32009757@gmail.com>
2021-06-19 23:23:23 +0900

    20210619

--- src/pages/user/edit-profile.tsx
+++ src/pages/user/edit-profile.tsx
@@ -4,6 +4,7 @@ import { useForm } from "react-hook-form";
 import { Button } from "../../components/button";
 import { useMe } from "../../hooks/useMe";
 import { editProfile, editProfileVariables } from "../../__generated__/editProfile";
+import {Helmet} from "react-helmet-async";
 
 const EDIT_PROFILE_MUTATION = gql`
     mutation editProfile($input:EditProfileInput!){
@@ -49,6 +50,9 @@ export const EditProfile = () => {
   }
   return (
     <div className="mt-52 flex flex-col justify-center items-center">
+      <Helmet>
+        <title>Edit Profile | Nuber Eats</title>
+      </Helmet>
       <h4 className="font-semibold text-2xl mb-3">Edit Profile</h4>
       <form onSubmit={handleSubmit(onSubmit)} className="grid max-w-screen-sm gap-3 mt-5 w-full mb-5">
         <input {...register('email', {required: "Email is required", pattern: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/ })} className="input" type="email" placeholder="Email" />
--- src/routers/logged-in-router.tsx
+++ src/routers/logged-in-router.tsx
@@ -1,14 +1,15 @@
 import React from "react";
 import {
   BrowserRouter as Router,
-  Redirect,
   Route,
   Switch,
 } from "react-router-dom";
 import { Header } from "../components/header";
 import { useMe } from "../hooks/useMe";
 import { NotFound } from "../pages/client/404";
+import { Category } from "../pages/client/category";
 import { Restaurants } from "../pages/client/restaurants";
+import { Search } from "../pages/client/search";
 import { ConfirmEmail } from "../pages/user/confirm-email";
 import { EditProfile } from "../pages/user/edit-profile";
 
@@ -21,6 +22,12 @@ const ClientRoutes = [
   </Route>,
   <Route key={3} path="/edit-profile" exact>
     <EditProfile />
+  </Route>,
+  <Route key={4} path="/search" exact>
+    <Search />
+  </Route>,
+  <Route key={5} path="/category/:slug" exact>
+    <Category />
   </Route>,
 ];
 
@@ -38,10 +45,10 @@ export const LoggedInRouter = () => {
       <Header />
       <Switch>
         {data.me.role === "Client" && ClientRoutes}
+        <Route>
+          <NotFound/>
+        </Route>
       </Switch>
-      <Route>
-        <NotFound />
-      </Route>
     </Router>
   );
 };
